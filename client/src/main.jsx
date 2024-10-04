46b6321b3e3a48d015ae554eb6cfe15939e1eda4
= <bottenbergdave@gmail.com>
2024-10-04 08:00:04 -0400

    added react router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Login from './login.jsx'
+import Signup from './Routes/signup.jsx'
+import './App.css'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <Login/>,
+  },
+  {
+    path:'/signup',
+    element: <Signup/>,
+  },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router = {router} />
   </StrictMode>,
 )
