393e90ac7966006a35b373e4040addcd22327d35
= <bottenbergdave@gmail.com>
2024-10-05 21:57:43 -0400

    no update

--- server/app.js
+++ server/app.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const app = express();
+const apiRoute = require('./routes/api');
 
 app.use(express.urlencoded({extended:true}));
+app.use('/api',apiRoute);
 
 app.listen(8080, ()=> console.log('server is running on port 8080'));
