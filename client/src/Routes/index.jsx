6c8a64eb46887c654c8ceee989cade9564a814ac
= <bottenbergdave@gmail.com>
2024-10-08 08:55:25 -0400

    formatting and styling the homepage

--- client/src/Routes/index.jsx
+++ client/src/Routes/index.jsx
@@ -16,7 +16,7 @@ const Index = () => {
   }, []);
 
   return (
-    <div>
+    <div className="pageCont">
       <h2>Welcome to FileStorage: {user.username} </h2>
       <div className="header">
         <div className="searchbar">
@@ -36,12 +36,63 @@ const Index = () => {
           <button>Box</button>
         </div>
       </div>
-      <div>
-        <select name ="val">
-            <option value="option1">Option1</option>
-            <option value="option2">Option2</option>
+      <div className="subHeader">
+        <select name ="displayType one">
+            <option value="All">All</option>
+            <option value="PDF">PDF</option>
+            <option value="Document">Document</option>
+            <option value="PowerPoint">PowerPoint</option>
+            <option value="Excel">Excel</option>
+            <option value="textFile">Text File</option>
+            <option value="JPG">JPG</option>
+            <option value="PNG">PNG</option>
+            <option value="GIF">GIF</option>
         </select>
+        <select name ="people two">
+            <option value="any">Any</option>
+            <option value="onlyMe">onlyMe</option>
+            <option value="shared">Shared With Me</option>
+            <option value="anyone">Anyone</option>
+        </select>
+        <select name ="modified three">
+            <option value="anytime">Anytime</option>
+            <option value="today">Today</option>
+            <option value="week">This Week</option>
+            <option value="month">This Month</option>
+        </select>
+
+        <div className="submitFilters four">Apply Filter</div>
       </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Last Opened</th>
+            <th>Owner</th>
+            <th>Location</th>
+            <th>File Size</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Sample.txt</td>
+            <td>10/08/2024</td>
+            <td>David Bottenberg</td>
+            <td>user/botte/odinproject/fileuploader/client/sample.txt</td>
+            <td>24kb</td>
+            <td>buttons</td>
+          </tr>
+          <tr>
+            <td>Sample.txt</td>
+            <td>10/08/2024</td>
+            <td>David Bottenberg</td>
+            <td>user/botte/odinproject/fileuploader/client/sample.txt</td>
+            <td>24kb</td>
+            <td>buttons</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
