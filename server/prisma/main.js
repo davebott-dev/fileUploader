25a8782def4bd16d50d815fcf39f249265060d28
= <bottenbergdave@gmail.com>
2024-10-12 10:50:43 -0400

    updated prisma schema to handle deletes

--- server/prisma/main.js
+++ server/prisma/main.js
@@ -2,16 +2,8 @@ const {PrismaClient} = require('@prisma/client');
 const prisma = new PrismaClient();
 
 async function main() {
-    const data = await prisma.post.create({
-        data: {
-            fileName: 'file',
-            size: 123,
-            data: 'data',
-            location: 'location',
-            authorId: 'f7559d58-13b5-46f6-b9a7-6b494e042e03'
-        }
-    });
-    console.log(data)
+    const data = await prisma.post.deleteMany({});
+    console.log(data);
 }
 
 main();
